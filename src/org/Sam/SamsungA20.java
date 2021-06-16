ebf94bda59c782456350c0152cdd319aed9e78ee
Chandrika-Nedunchezhian <chandrikanedunchezhian@gmail.com>
2021-06-16 14:15:30 +0530

    model

--- src/org/Sam/SamsungA20.java
+++ src/org/Sam/SamsungA20.java
@@ -4,6 +4,9 @@ public class SamsungA20 {
 
 	public static void main(String[] args) {
 		System.out.println("SAMSUNG");
+		System.out.println("A20");
+		System.out.println("M20");
+		System.out.println("M21");
 	}
 	
 }
